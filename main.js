abd6aaf841eb350fe15b54588d7655bd4232d4ba
Spider0607 <hoachau060798@gmail.com>
2023-12-06 19:40:47 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 6,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      500: {
+        items: 3,
+      },
+    },
+  });
+});
